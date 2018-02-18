f082e6f29fc69fa0849c986e8f86feb7d48765cf
yowchun <yowchun93@gmail.com>
2018-02-18 11:57:15 +0800

    added Person component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var Person = require('./Person.js')
 
 class App extends Component {
   render() {
@@ -10,9 +11,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> Testing React </h1>
+        <button> Switch Name </button>
+        <Person name="Yc"/>
+        <Person name="Vinz"/>
       </div>
     );
   }
---
+++ src/Person.js
@@ -0,0 +1,8 @@
+var React = require('react');
+
+const Person = function(props){
+  return <h1> Hello , {props.name} </h1>
+}
+
+module.exports = Person
+
